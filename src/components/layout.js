70cfab2fc61371126176ae1067a355caafaf0098
Zachary Newton <zacharyrnewton@gmail.com>
2019-10-16 12:55:42 -0400

    fixed window resize jank

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,6 +13,17 @@ import Navigation from "./navigation"
 import Footer from "./footer"
 import "../sass/config/config.sass"
 
+// Window Resize Animation Stopper
+if (typeof document !== 'undefined') {
+  let resizeTimer;
+  window.addEventListener("resize", () => {
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+      document.body.classList.remove("resize-animation-stopper");
+    }, 400);
+  });
+};
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
