609dc9c0a5b2897734798a8f7cac374ed3c3675f
Zachary Newton <zacharyrnewton@gmail.com>
2019-10-25 11:28:42 -0400

    Removed animation.

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,15 @@ const IndexPage = () => (
         overline={"AH Blockchain"}
         title={"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam"}
         body={"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd."}
-        />
+      />
+      <CardInfo
+      />
+      <CardInfo
+      />
+      <CardInfo
+      />
+      <CardInfo
+      />
     </section>
   </Layout>
 )
